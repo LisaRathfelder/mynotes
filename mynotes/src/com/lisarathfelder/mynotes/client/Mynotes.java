d0f5fbe54c48471c0f0727fbbb9352ce6b25a2a0
Lisa <Lisa@192.168.2.107>
2018-04-09 22:16:21 +0200

    Panels, Buttons erstellt, clickhandler für login- und logoutbutton erstellt, Panels angepasst

--- mynotes/src/com/lisarathfelder/mynotes/client/Mynotes.java
+++ mynotes/src/com/lisarathfelder/mynotes/client/Mynotes.java
@@ -3,6 +3,7 @@ package com.lisarathfelder.mynotes.client;
 import com.lisarathfelder.mynotes.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.impl.AsyncFragmentLoader.Logger;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -12,6 +13,8 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -33,10 +36,14 @@ public class Mynotes implements EntryPoint {
 	 */
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
+	private  String username;
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		
+		
 		final Button sendButton = new Button("Old button to be deleted");
 		final TextBox nameField = new TextBox();
 		nameField.setText("old old old");
@@ -50,6 +57,21 @@ public class Mynotes implements EntryPoint {
 		final VerticalPanel notePanel=new VerticalPanel();
 		final VerticalPanel editPanel=new VerticalPanel();
 		
+		loginPanel.setWidth("100%");
+		loginPanel.setHeight("100%");
+		loginPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		loginPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		
+		notePanel.setWidth("100%");
+		notePanel.setHeight("100%");
+		notePanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		notePanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		
+		editPanel.setWidth("100%");
+		editPanel.setHeight("100%");
+		editPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		editPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		
 		//Elemente von Login View
 		final TextBox usernameField = new TextBox();
 		usernameField.setText("Please type your username");
@@ -59,13 +81,34 @@ public class Mynotes implements EntryPoint {
 		
 		loginPanel.add(usernameField);
 		loginPanel.add(loginButton);
+
 		
 		//Elemente von Note View
+		final Button createButton = new Button("Create note");
+		final TextBox noteText = new TextBox();
+		noteText.setText("Saved Note1");
+		final Button editButton = new Button("Edit");
+		final Button deleteButton = new Button("Delete");
+		final Button logoutButton = new Button("Logout");
+		
+		logoutButton.getElement().setClassName("button");
+		
+		notePanel.add(createButton);
+		//notePanel.add(noteText);
+		//notePanel.add(editButton);
+		//notePanel.add(deleteButton);
+		notePanel.add(logoutButton);
+		
+		
 		
 		//Elemente von Edit View
+		//final TextArea 
+		final TextBox editText = new TextBox();//
+		editText.setText("Type your Note / Existing Note comes here");
+		final Button saveButton = new Button("Save");
 		
-		
-		
+		editPanel.add(editText);
+		editPanel.add(saveButton);
 		
 		
 		RootPanel.get("mainContainer").add(loginPanel);
@@ -74,6 +117,36 @@ public class Mynotes implements EntryPoint {
 		loginPanel.setVisible(true);
 		notePanel.setVisible(false); //soll nicht sichtbar sein 
 		editPanel.setVisible(false);
+		
+		// Event handlers:
+		
+		// Add a handler to login button
+		loginButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				loginPanel.setVisible(false);
+				notePanel.setVisible(true); //soll nicht sichtbar sein 
+				editPanel.setVisible(false);
+				username=usernameField.getText();
+				noteText.setText("Saved Note1 of " + username);
+
+			}
+		});
+	
+		
+		// Add a handler to logout button
+		logoutButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				loginPanel.setVisible(true);
+				notePanel.setVisible(false); //soll nicht sichtbar sein 
+				editPanel.setVisible(false);
+				usernameField.setText(username);
+			}
+		});	
+		
+		
+		
+		
+		
 		
 
 		// Focus the cursor on the name field when the app loads
